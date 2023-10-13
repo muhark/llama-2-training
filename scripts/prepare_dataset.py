33a255bd90547a9802370ef5693b7b9a90914b09
drmjh <mj2976@princeton.edu>
2023-10-13 09:58:48 -0400

    Changed preparation for consistency with HF CLM script

--- scripts/prepare_dataset.py
+++ scripts/prepare_dataset.py
@@ -1,23 +1,24 @@
 # Script for converting propaganda data to HF datasets format needed by training script
+# %%
 import os
 import json
 import datasets
 from pathlib import Path
 from transformers import AutoTokenizer
 
-base_dir = Path("/projects/BSTEWART/mj2976/llama-2-training")
+base_dir = Path("/scratch/gpfs/mj2976/projects/llama-2-training")
 data_dir = base_dir / "data"
 data_file = data_dir / "prop_articles.json"
-data = json.loads(data_file.read_text().splitlines())
-# or
-data = [json.loads(record) for record in data_file.read_text().splitlines()]
+
 
 # Initial construction
-ds = datasets.Dataset.from_json(str(data_file))
-ds.save_to_disk(str(data_dir/"prop_articles_nosplit"))
+original_ds = datasets.load_dataset('json', data_files=str(data_file))
+
+# ds = datasets.Dataset.from_json(str(data_file))
+# ds.save_to_disk(str(data_dir/"prop_articles_nosplit"))
 
 # Create splits
-train_testvalid = ds.train_test_split(test_size=0.1, shuffle=True, seed=42)
+train_testvalid = original_ds['train'].train_test_split(test_size=0.1, shuffle=True, seed=42)
 test_valid = train_testvalid['test'].train_test_split(test_size=0.5, shuffle=True, seed=42)
 train_test_valid_ds = datasets.DatasetDict({
     'train': train_testvalid['train'],
@@ -25,6 +26,17 @@ train_test_valid_ds = datasets.DatasetDict({
     'valid': test_valid['train']
 })
 train_test_valid_ds.save_to_disk(str(data_dir/"prop_articles"))
+
+# Remove whitespaces
+train_test_valid_ds.map(lambda row: {
+        'text': row['content_cl'].replace(' ', ''),
+        'id': row['id']},
+        remove_columns=['newspaper', 'title', 'date', 'content', 'type', 'content_cl', 'cluster_name'])
+
+ws = ' '
+
+
+
 
 # Tokenize data
 def get_weight_dir(
